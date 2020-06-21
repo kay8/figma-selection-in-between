4a3eed5f4cfa22720d513aa50f777e984eb6a205
Kei Yamagishi <kay.yamagishi.work@gmail.com>
2020-06-21 17:23:03 +0900

    set up lint and prettier, and clean up codes

--- src/code.ts
+++ src/code.ts
@@ -1,24 +1,24 @@
-figma.showUI(__html__)
+figma.showUI(__html__);
 
 figma.ui.onmessage = msg => {
   if (msg.type === 'create-rectangles') {
-    createRectangle(msg.count)
+    createRectangle(msg.count);
   }
 
-  figma.closePlugin()
-}
+  figma.closePlugin();
+};
 
-function createRectangle(count) {
-  const nodes = []
+function createRectangle(count: number) {
+  const nodes = [];
 
-  for (let i = 0; i < count; i++) {
-    const rect = figma.createRectangle()
-    rect.x = i * 150
-    rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}]
-    figma.currentPage.appendChild(rect)
-    nodes.push(rect)
+  for (let i = 0; i < count; i += 1) {
+    const rect = figma.createRectangle();
+    rect.x = i * 150;
+    rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+    figma.currentPage.appendChild(rect);
+    nodes.push(rect);
   }
 
-  figma.currentPage.selection = nodes
-  figma.viewport.scrollAndZoomIntoView(nodes)
+  figma.currentPage.selection = nodes;
+  figma.viewport.scrollAndZoomIntoView(nodes);
 }
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import './ui.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './ui.css';
 
-declare function require(path: string): any
+declare function require(path: string): any;
 
-const App: React.FC = ({}) => {
-  const textbox = React.useRef<HTMLInputElement>(undefined);
+const App: React.FC = () => {
+  const textbox = React.useRef<HTMLInputElement | null>(null);
 
   const countRef = React.useCallback((element: HTMLInputElement) => {
     if (element) element.value = '5';
@@ -13,38 +13,31 @@ const App: React.FC = ({}) => {
   }, []);
 
   const onCreate = React.useCallback(() => {
-      const count = parseInt(textbox.current.value, 10);
-      parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
+    if (!textbox.current) return;
+    const count = parseInt(textbox.current.value, 10);
+    parent.postMessage(
+      { pluginMessage: { type: 'create-rectangles', count } },
+      '*'
+    );
   }, []);
 
   const onCancel = React.useCallback(() => {
-      parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-  }, []);
-
-  const OnTest = React.useCallback(() => {
-    parent.postMessage({pluginMessage: {type: 'test'}}, '*');
-  }, []);
-
-  React.useEffect(() => {
-    // This is how we read messages sent from the plugin controller
-    window.onmessage = (event) => {
-        const { type, message } = event.data.pluginMessage;
-        if (type === 'create-rectangles') {
-            console.log(`Figma Says: ${message}`);
-        };
-    }
+    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
   }, []);
 
   return (
     <div>
       <img src={require('./logo.svg')} />
       <h2>Rectangle Creator</h2>
-      <p>Count: <input ref={this.countRef} /></p>
-      <button id="create" onClick={this.onCreate}>Create</button>
-      <button id="create" onClick={this.onCreate}>Create</button>
-      <button onClick={this.OnTest}>Test</button>
+      <p>
+        Count: <input ref={countRef} />
+      </p>
+      <button className="button__action" onClick={onCreate}>
+        Create
+      </button>
+      <button onClick={onCancel}>Cancel</button>
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('react-page'))
+ReactDOM.render(<App />, document.getElementById('react-page'));
